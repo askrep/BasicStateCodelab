686b937b13c7e3313a5a3d3f82158d5d87e1f05a
askrep <alexandskr@gmail.com>
2022-05-31 17:19:58 +0300

    #6 State driven UI

--- app/src/main/java/com/kas/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/kas/basicstatecodelab/WaterCounter.kt
@@ -4,10 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,16 +12,15 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-
     Column(modifier.padding(16.dp)) {
-        val count: MutableState<Int> = remember { mutableStateOf(0) }
-
-        Text(
-            text = "You've had ${count.value} glasses",
-            modifier = modifier.padding(16.dp)
-        )
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            Text(
+                text = "You've had $count glasses"
+            )
+        }
         Button(
-            onClick = { count.value++ },
+            onClick = { count++ },
             modifier = modifier.padding(8.dp)
         ) {
             Text(text = "Add one")
